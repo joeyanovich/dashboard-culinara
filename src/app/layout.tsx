38b0a1ad55b5c52ec7eae5d150149e0ec84cd118
joeyanovich <joedison.dias@gmail.com>
2024-07-19 22:18:52 -0300

    clean: remove initial files

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,10 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({
+  weight: ['300', '400', '500', '600'],
+  subsets: ["latin"]
+});
 
 export const metadata: Metadata = {
   title: "Create Next App",
